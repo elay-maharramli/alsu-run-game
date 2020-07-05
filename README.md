57a6244cf76f333d7cdc83c3bfd64804e1069c16
ElaySuperCoder <36122259+Maharramli@users.noreply.github.com>
2020-07-05 09:39:48 +0400

    update: readme change

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/09d8fc22-d87a-4e5d-9573-d680c4db02fe/deploy-status)](https://alsu-run.netlify.com/)
+[![Netlify Status](https://api.netlify.com/api/v1/badges/09d8fc22-d87a-4e5d-9573-d680c4db02fe/deploy-status)](https://run-game.netlify.com/)
--- index.js
+++ index.js
@@ -6,33 +6,25 @@ class Helper
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-
     static mouseLeftClick(evt)
     {
         let flag = false;
-
         evt = evt || window.event;
-
         if ('buttons' in evt)
         {
             flag = evt.buttons === 1;
         }
-
         if (!flag)
         {
             let button = evt.which || evt.button;
-
             flag = button === 1;
         }
-
         return flag;
     }
-
     static _timestamp()
     {
         return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
     }
-
     /*
     Delete an element from an array without
     having to create a new array in the process
@@ -44,20 +36,17 @@ class Helper
         {
             return;
         }
-
         array[index] = array[array.length - 1];
         array[array.length - 1] = undefined;
         array.length = array.length - 1;
     }
 }
-
 class Sound
 {
     constructor(src)
     {
         this.audio = new Audio(src);
     }
-
     play()
     {
         this.audio.pause();
@@ -65,7 +54,6 @@ class Sound
         this.audio.play().then(() => {}).catch(() => {})
     }
 }
-
 class Canvas
 {
     constructor(width, height)
@@ -77,7 +65,6 @@ class Canvas
         this.context = this.canvas.getContext('2d');
     }
 }
-
 class Background
 {
     constructor(canvas, src)
@@ -88,7 +75,6 @@ class Background
         this.scrollSpeed = 6;
         this.img.src = src;
     }
-
     draw()
     {
         // Resetting the images when the first image exits the screen
@@ -98,16 +84,13 @@ class Background
         }
         // Update background X position
         this.scrollX += this.scrollSpeed;
-
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
         this.ctx.drawImage(this.img, -this.scrollX, 0, this.ctx.canvas.width, this.ctx.canvas.height);
         this.ctx.drawImage(this.img, this.ctx.canvas.width - this.scrollX, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 }
-
 class Balloon
 {
-
     constructor(x, y, dx, dy, color, context)
     {
         this.y = y;
@@ -121,13 +104,11 @@ class Balloon
         this.img = new Image();
         this.img.src = 'img/balloon-' + color + '.png';
     }
-
     update()
     {
         this.x += this.dx;
         this.y += this.dy;
     }
-
     draw()
     {
         this.ctx.drawImage(
@@ -135,9 +116,7 @@ class Balloon
             this.x, this.y, //The destination x and y position
             this.w, this.h //The destination height and width
         );
-
     }
-
     outOfScreen()
     {
         return this.x + this.w < 0 || this.x > this.ctx.canvas.width || this.y > this.ctx.canvas.height;
@@ -158,37 +137,31 @@ class Box
         this.img = new Image();
         this.img.src = 'img/obstacle.jpg';
     }
-
     update()
     {
         this.x -= this.dx;
         this.y -= this.dy;
     }
-
     draw()
     {
         this.ctx.drawImage(
-          this.img,
-          this.x, this.y,
-          this.w, this.h
+            this.img,
+            this.x, this.y,
+            this.w, this.h
         );
     }
-
     outOfScreen()
     {
         return this.x + this.w < 0 || this.x > this.ctx.canvas.width || this.y > this.ctx.canvas.height;
     }
-
     collidesWith(object)
     {
         return this.distanceBetween(object) < (this.radius + object.radius);
     }
-
     distanceBetween(object)
     {
         return Math.sqrt(Math.pow(this.x - object.x, 2) + Math.pow(this.y - object.y, 2));
     }
-
 }
 class Spike
 {
@@ -204,13 +177,11 @@ class Spike
         this.img = new Image();
         this.img.src = 'img/spike.png';
     }
-
     update()
     {
         this.x -= this.dx;
         this.y -= this.dy;
     }
-
     draw()
     {
         this.ctx.drawImage(
@@ -218,24 +189,19 @@ class Spike
             this.x, this.y,
             this.w, this.h
         )
-
     }
-
     outOfScreen()
     {
         return this.x + this.w < 0 || this.x > this.ctx.canvas.width || this.y > this.ctx.canvas.height;
     }
-
     collidesWith(object)
     {
         return this.distanceBetween(object) < (this.radius + object.radius);
     }
-
     distanceBetween(object)
     {
         return Math.sqrt(Math.pow(this.x - object.x, 2) + Math.pow(this.y - object.y, 2));
     }
-
 }
 class Player
 {
@@ -249,13 +215,11 @@ class Player
             { x: 306, y: 140, w: 68, h: 101 },
             { x: 382, y: 141, w: 68, h: 99 },
         ];
-
         this.jumpSprites = [
             { x: 23, y: 265, w: 65, h: 105 },
             { x: 97, y: 264, w: 78, h: 106 },
             { x: 184, y: 272, w: 78, h: 97 },
         ];
-
         this.groundY = groundY;
         this.ctx = context;
         this.h = 100;
@@ -263,25 +227,19 @@ class Player
         this.y = this.ctx.canvas.height - this.groundY - this.h;
         this.dy = 0;
         this.radius = 40;
-
         this.img = new Image();
         this.img.src = 'img/alsu.png';
-
         this.jumpSound = new Sound('sound/jump.mp3');
-
         this.timer = 0;
         this.nextFrame = 0;
         this.frameInterval = 5;
-
         this.jumpHeight = 22;
         this.grounded = true;
         this.gravity = 1;
     }
-
     update()
     {
         this.y += this.dy;
-
         // Gravity
         if (this.y + this.h < this.ctx.canvas.height - this.groundY)
         {
@@ -295,7 +253,6 @@ class Player
             this.y = this.ctx.canvas.height - this.groundY - this.h;
         }
     }
-
     draw()
     {
         if (this.grounded)
@@ -304,7 +261,6 @@ class Player
             {
                 this.nextFrame = 0;
             }
-
             this.ctx.drawImage(
                 this.img,
                 this.runSprites[this.nextFrame].x, this.runSprites[this.nextFrame].y,
@@ -312,13 +268,11 @@ class Player
                 this.x, this.y,
                 this.runSprites[this.nextFrame].w, this.runSprites[this.nextFrame].h
             );
-
             if (this.timer > this.frameInterval)
             {
                 this.timer = 0;
                 this.nextFrame++;
             }
-
             this.timer++;
         }
         else
@@ -332,7 +286,6 @@ class Player
             );
         }
     }
-
     jump()
     {
         if (this.grounded)
@@ -342,18 +295,15 @@ class Player
             this.grounded = false;
         }
     }
-
     collidesWith(object)
     {
         return this.distanceBetween(object) < (this.radius + object.radius);
     }
-
     distanceBetween(object)
     {
         return Math.sqrt(Math.pow(this.x - object.x, 2) + Math.pow(this.y - object.y, 2));
     }
 }
-
 class Game
 {
     constructor(canvas)
@@ -365,7 +315,6 @@ class Game
         this.now = 0;
         this.lastTime = Helper._timestamp();
         this.deltaTime = 0;
-
         this.ctx = canvas.context;
         this.background = new Background(canvas, 'img/bg.png');
         this.groundY = 84;
@@ -389,65 +338,51 @@ class Game
         this.spikeSpeed = 7;
         this.boxSpeed = 7;
         this.score = 0;
-
     }
-
     start()
     {
-
         if (this.gameRunning)
         {
             return false;
         }
-
         document.getElementById('game-starter').style.display = 'none';
         document.getElementById('game-stats').style.display = 'block';
         this.gameRunning = true;
         this._mouseLeftClickListener();
         this._animate();
     }
-
     _animate()
     {
         if (this.gamePaused)
         {
             return;
         }
-
         this.now = Helper._timestamp();
         this.deltaTime = this.deltaTime + Math.min(1, (this.now - this.lastTime) / 1000);
-
         while (this.deltaTime > this.step)
         {
             this.deltaTime = this.deltaTime - this.step;
             this._create(this.step);
             this._update(this.step);
         }
-
         this._draw(this.deltaTime);
         this.lastTime = this.now;
-
         requestAnimationFrame(() => this._animate());
     }
-
     _create(step)
     {
         if (this.balloonTimer % this.balloonSpawnInterval === 0)
         {
-
             // Clone balloons again if its empty
             if (this.balloonColorsCopy.length === 0)
             {
                 this.balloonColorsCopy = [...this.balloonColors];
             }
-
             // Get unique balloon randomly
             let randomBalloonColorIdx = Math.floor(Math.random() * this.balloonColorsCopy.length);
             let randomBalloonColor = this.balloonColorsCopy[randomBalloonColorIdx];
-
             // Remove current generated balloon from copy
             this.balloonColorsCopy.splice(randomBalloonColorIdx, 1);
-
             this.balloons.push(new Balloon(
                 700,
                 Helper.getRandomInt(100, 200),
@@ -456,8 +391,6 @@ class Game
                 randomBalloonColor,
                 this.ctx
             ));
-
-
             this.balloonSpawnInterval = Helper.getRandomInt(100, 200);
             this.balloonTimer = 0;
         }
@@ -469,14 +402,12 @@ class Game
                 this.spikeSpeed,
                 this.spike.dy,
                 this.ctx
-                ));
-
+            ));
             this.spikeSpawnInterval = Helper.getRandomInt(1600,1500);
             this.spikeTimer = 0;
         }
         if (this.boxTimer % this.boxSpawnInterval === 0)
         {
-
             this.boxes.push(new Box(
                 900,
                 this.box.y,
@@ -484,13 +415,10 @@ class Game
                 this.box.dy,
                 this.ctx
             ));
-
-
             this.boxSpawnInterval = Helper.getRandomInt(1000,900)
             this.boxTimer = 0;
         }
     }
-
     _update(step)
     {
         // Update player position
@@ -499,17 +427,13 @@ class Game
         for (let i in this.balloons)
         {
             // Update balloon position
-
-
             this.balloons[i].update();
-
             if (this.balloons.hasOwnProperty(i) && this.player.collidesWith(this.balloons[i]))
             {
                 this.collectSound.play();
                 Helper.removeIndex(this.balloons, i);
                 this._scoreUpdate();
             }
-
             // Remove the balloon if out of screen.
             if (this.balloons.hasOwnProperty(i) && this.balloons[i].outOfScreen())
             {
@@ -518,10 +442,8 @@ class Game
         }
         this.balloonTimer++;
         this.boxTimer++;
-
         for (let a in this.boxes)
         {
-
             this.boxes[a].update();
 
             if (this.boxes.hasOwnProperty(a) && this.player.collidesWith(this.boxes[a]))
@@ -531,19 +453,13 @@ class Game
                 this.boxCollideSound.play();
                 throw new Error("GAME OVER!");
             }
-
             if (this.boxes.hasOwnProperty(a) && this.boxes[a].outOfScreen())
             {
                 Helper.removeIndex(this.boxes, a);
             }
-
-
-
         }
-
         for (let s in this.spikes)
         {
-
             this.spikes[s].update();
 
             if (this.spikes.hasOwnProperty(s) && this.player.collidesWith(this.spikes[s]))
@@ -553,18 +469,14 @@ class Game
                 document.getElementById("game-over").style.display = "block";
                 throw new Error("GAME OVER!");
             }
-
             // Remove the balloon if out of screen.
             if (this.spikes.hasOwnProperty(s) && this.spikes[s].outOfScreen())
             {
                 Helper.removeIndex(this.spikes, s);
             }
         }
-
         this.spikeTimer++;
-
     }
-
     _draw(dt)
     {
         this.background.draw();
@@ -585,7 +497,6 @@ class Game
             this.spikes[s].draw();
         }
     }
-
     _mouseLeftClick(event)
     {
         if (Helper.mouseLeftClick(event))
@@ -593,7 +504,6 @@ class Game
             this.player.jump();
         }
     }
-
     _mouseLeftClickListener()
     {
         let self = this;
@@ -602,13 +512,11 @@ class Game
             self._mouseLeftClick(event);
         });
     }
-
     _scoreUpdate()
     {
         document.getElementById('game-score').innerText = '' + ++this.score;
         document.getElementById('game-score').innerText = '' + ++this.score;
     }
 }
-
 let canvas = new Canvas(950, 500);
 let game = new Game(canvas);
